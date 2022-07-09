3c62301807d48f22bcd4b52c2c54326cb2a5586b
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-07-09 19:15:46 +0300

    Initial commit for my_hibernate_project

---
+++ src/main/java/ua/ithillel/my_hibernate_project/MyHibernateProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.ithillel.my_hibernate_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHibernateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHibernateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/ithillel/my_hibernate_project/MyHibernateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ithillel.my_hibernate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHibernateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
